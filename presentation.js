d97d9f99812e4d6b3faf5cb2e61ceea8bd5b3b92
Sumaya Al Asiri <sumayaalaseri@gmail.com>
2026-03-01 10:12:07 +0300

    Updated presentation layer after removing assign feature

---
+++ presentation.js
@@ -0,0 +1,79 @@
+const business = require('./business.js')
+const prompt=require('prompt-sync')()
+
+
+/**
+ * A function to interact with the user and display the results of the
+ * employee schedule in a CSV like format.
+ */
+async function getEmployeeSchedule() {
+    let empId = prompt('Enter employee ID: ')
+    let details = await business.getEmployeeShifts(empId)
+    console.log('\n')
+    console.log('date,start,end')
+    for (let d of details) {
+        console.log(`${d.date},${d.startTime},${d.endTime}`)
+    }
+}
+
+/**
+ * Display the employee list in a nicely formatted table.
+ */
+async function displayEmployees() {
+    let employees = await business.getAllEmployees()
+    console.log('Employee ID  Name                Phone')
+    console.log('-----------  ------------------- ---------')
+    for (let emp of employees) {
+        console.log(`${emp.employeeId.padEnd(13)}${emp.name.padEnd(20)}${emp.phone}`)
+    }
+}
+
+/**
+ * The UI function for adding a new employee to the system.
+ */
+async function addNewEmployee() {
+    let name = prompt('Enter employee name: ')
+    let phone = prompt('Enter phone number: ')
+    await business.addEmployeeRecord({
+        name: name,
+        phone: phone
+    })
+    console.log('Employee added...')
+}
+
+
+/**
+ * The UI function for displaying the menu and calling the various UI functions.  The function
+ * is made async because many of the called functions are also async.
+ */
+async function displayMenu() {
+    while (true) {
+        console.log('1. Show all employees')
+        console.log('2. Add new employee')
+        console.log('3. View employee schedule')
+        console.log('4. Exit')
+        let choice = Number(prompt("What is your choice> "))
+        if (choice === 1) {
+            await displayEmployees()
+            console.log('\n\n')
+        }
+        else if (choice == 2) {
+            await addNewEmployee()
+            console.log('\n\n')
+        }
+        else if (choice == 3) {
+            await getEmployeeSchedule()
+            console.log('\n\n')
+        }
+        else if (choice == 4) {
+            break
+        }
+        else {
+            console.log("Error in selection")
+        }
+    }
+    console.log('*** Goodbye!')
+}
+
+displayMenu()
+
